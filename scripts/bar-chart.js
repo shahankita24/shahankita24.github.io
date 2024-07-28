cac7e0a3242deb5ee7d8a9d6726f1bdb09d9bdef
ankita082 <shahankita082@gmail.com>
2024-07-27 18:15:20 -0600

    Display dropdown only on bar chart

--- scripts/bar-chart.js
+++ scripts/bar-chart.js
@@ -56,7 +56,7 @@ function createScene3(data) {
         .style("opacity", 0);
 
     // Append dropdown directly into SVG
-    const dropdown = svg.append("g").
+    const dropdown = d3.select("#chart3").append("svg")
         .append("select")
         .attr("class", "svg-dropdown")
         .style("position", "absolute")
